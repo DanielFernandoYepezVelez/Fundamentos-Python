de4bc130db153759a31486d7cdd2419d43c7e3a9
DanielFernandoYepezVelez <danipez.02@gmail.com>
2020-08-06 17:52:09 -0500

    Aplicando Mejoras En La Estructura Principal E Incluyendo El Modulo De Los Operadores

---
+++ 03-Operadores/01-Aritmeticos.py
@@ -0,0 +1,20 @@
+""" OPERADOR ASIGNACIÓN """
+numeroUno = 5
+numeroDos = 12
+restoUno = 8
+restoDos = 4
+
+""" OPERADORES ARITMETICOS """
+suma = numeroUno + numeroDos
+resta = numeroDos - numeroUno
+producto = numeroUno * numeroDos
+division = numeroDos / numeroUno
+moduloRestoImpar = numeroDos / numeroUno
+moduloRestoPar = restoUno % restoDos
+
+print(f'Suma => {suma}')
+print(f'Resta => {resta}')
+print(f'Producto => {producto}')
+print(f'Division => {division}')
+print(f'Modulo / Resto => {moduloRestoImpar}')
+print(f'Modulo / Resto => {moduloRestoPar}')
---
+++ 03-Operadores/02-Asignacion.py
@@ -0,0 +1,18 @@
+""" OPERADORES DE ASIGNACIÓN """
+edad = 40
+print(f'Edad => {edad}')
+
+edad += 5
+print(f'Edad => {edad}')
+
+edad -= 5
+print(f'Edad => {edad}')
+
+edad *= 2
+print(f'Edad => {edad}')
+
+edad /= 4
+print(f'Edad => {edad}')
+
+edad %= 10
+print(f'Edad => {edad}')
