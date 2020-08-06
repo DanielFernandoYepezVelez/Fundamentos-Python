b0334580fc7e52d5cadce7d329fc467cfe8f30b3
DanielFernandoYepezVelez <danipez.02@gmail.com>
2020-08-06 18:14:38 -0500

    Terminando El Tema De Los Operadores

---
+++ 03-Operadores/03-Incremento.py
@@ -0,0 +1,14 @@
+""" ESTE TIPO DE OPERADOR EN PYTHON NO EXISTE, 
+PERO SU EQUIVALENTE PODRIA SER EL SIGUIENTES: """
+year = 10
+
+# INCREMENTO
+year = year + 1
+print(f'Incremento => {year}')
+
+year += 1
+print(f'Incremento => {year}')
+
+# PRE-INCREMENTO
+year = 1 + year
+print(f'Pre-Incremento => {year}')
---
+++ 03-Operadores/04-Decremento.py
@@ -0,0 +1,14 @@
+""" ESTE TIPO DE OPERADOR EN PYTHON NO EXISTE, 
+PERO SU EQUIVALENTE PODRIA SER EL SIGUIENTES: """
+year = 10
+
+# DECREMENTO
+year = year - 1
+print(f'Decremento => {year}')
+
+year -= 1
+print(f'Decremento => {year}')
+
+# PRE-DECREMENTO
+year = 1 - year
+print(f'Pre-Decremento => {year}')
