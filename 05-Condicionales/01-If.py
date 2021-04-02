95ab7dfe28a251854b40d1e2728dd0f8f0ac7188
danielfernandoyepezvelez <danielfernandoyepezvelez>
2021-04-02 15:08:26 -0500

    Mejorando La Seccion De Condicionales, Simples, Compuestas, Anidadas

--- 05-Condicionales/01-If.py
+++ 05-Condicionales/01-If.py
@@ -1,5 +1,7 @@
 # Condicional IF
 
+SEPARADOR = '-----------------'
+
 """ EJEMPLO #1 """
 # color = input('¿Ingresa Color Favorito?\n')
 color = 'rojo'
@@ -9,7 +11,8 @@ if color == 'rojo':
 else:
     print('Color NO Es Rojo')
 
-print('#########\n')
+print(f'{SEPARADOR}\n')
+
 """ EJEMPLO #2 """
 year = int(input('¿Ingresa En Año Estamos?\n'))
 # year = 2020
@@ -18,3 +21,21 @@ if year >= 2021:
     print(f'Estamos De 2021 En Adelante')
 else:
     print(f'Es Un Año Anterior A 2021')
+
+print(f'{SEPARADOR}\n')
+
+""" EJEMPLO #3 """
+lenguaje = 'python';
+
+if not lenguaje == 'python':
+    print(f'Excelente Decisión Aprendiendo {lenguaje}')
+else:
+    print(f'Mala Decisión NO! Aprendiendo {lenguaje}')
+
+print(f'{SEPARADOR}\n')
+
+""" EJEMPLO #4 """
+peliculas = ['PeliUno', 'PeliDos', 'PeliTres']
+
+if 'PeliUno' in peliculas:
+    print('La Pelicula Uno Si Existe En La Lista')
---
+++ 05-Condicionales/04-CondicionalesFor.py
@@ -0,0 +1,9 @@
+lenguajes = ['Python', 'Kotlin', 'Java', 'JavaScript']
+
+for lenguaje in lenguajes:
+    if lenguaje == 'Python':
+        print('Excelente, Python!!!')
+        print('Método Para Strings UPPER(): ' + lenguaje.upper())
+        print('--------------\n')
+    else:
+        print(lenguaje)
