9585663ee5a2f0769bf674bb2b3c3d1a6dd1050e
danielfernandoyepezvelez <danielfernandoyepezvelez>
2021-05-28 11:50:01 -0500

    Agregando El Operador Ternanrio

---
+++ 05-Condicionales/05-Ternario.py
@@ -0,0 +1,8 @@
+""" Muestra El Mensaje Por Pantalla Si x == 5, Por El Contrario Muestra El else """
+x = 54
+print('X Es Igual a 5') if x == 5 else print('X No Es Igual a 5')
+
+""" Muestra El Valor De La Variable Si La Condición Se Cumple, Por El Contrario Muestra El else """
+es_bonito = False
+estado = "Es bonito" if es_bonito else "No es bonito"
+print(estado)
